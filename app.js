4ad28a300a11326696e2da346eaad65af7e96f85
Faruk Ekiz <f.ekiz@hotmail.nl>
2016-03-22 13:13:34 +0100

    02_04_The HttpClient

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+import {HttpClient} from "aurelia-http-client";
+
 export class App {
     
   constructor() {
     this.message = "";
+    this.http = new HttpClient();
   }
   
   activate(){
-      this.message = "Hello from Aurelia!";
-  }
+      this.message = "Hello from Aurelia!";  
+      return this.http.get("/movies.json")
+                      .then(response => {
+                          this.movies = response.content;
+                        });
+  } 
   
   changeMessage(){
       this.message = "Goodbye";
